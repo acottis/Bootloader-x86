da297fc9aa84aed0bd8b31e31602773e2a525bc0
Adam Cottis <adam.cottis@outlook.com>
2023-10-15 13:13:36 +0100

    Working without PIC

--- Makefile
+++ Makefile
@@ -2,11 +2,12 @@ default: run
 
 run: rust
 	qemu-system-i386 -fda target/boot.bin -boot order=a
+	# qemu-system-x86_64 -fda target/boot.bin -boot order=a
 
 asm:
 	nasm -felf32 boot.asm -o target/asm.o
 
 rust: asm
-	rustc --target i686-unknown-linux-gnu --emit=obj -C relocation-model=static -C panic=abort -C lto -C opt-level=3 src/lib.rs -o target/rust.o
+	rustc --target i686-unknown-linux-gnu --emit=obj -C relocation-model=static -C panic=abort -C lto -C opt-level=z src/lib.rs -o target/rust.o
 	ld target/asm.o target/rust.o -T link.ld -m elf_i386 -nmagic -o target/boot.bin
 
--- src/lib.rs
+++ src/lib.rs
@@ -7,18 +7,42 @@ use core::arch::asm;
 const BG_LIGHT_GREY: u8 = 0x07;
 static mut VGA_BUFFER: *mut u8 = 0xB8000 as *mut u8;
 
+// IDT STUFF
+const IDT_ENTRIES: u16 = 256;
+const IDT_SIZE: u16 = core::mem::size_of::<IdtEntry>() as u16;
+const IDT_LENGTH: u16 = IDT_ENTRIES * IDT_SIZE - 1;
+const CODE_SELECTOR_OFFSET: u16 = 8;
+
+/// PIC Stuff
+const PIC1: u16 = 0x20;
+const PIC1_COMMAND: u16 = PIC1;
+const PIC1_DATA: u16 = PIC1 + 1;
+
+const PIC2: u16 = 0xA0;
+const PIC2_COMMAND: u16 = PIC2;
+const PIC2_DATA: u16 = PIC2 + 1;
+
+const PIC_END_OF_INTERRUPT: u8 = 0x20;
+
+const ICW1_INIT: u8 = 0x10;
+const ICW1_ICW4: u8 = 0x01;
+
+const ICW4_8086: u8 = 0x01;
+
 #[panic_handler]
 fn panic_handler(_panic: &core::panic::PanicInfo<'_>) -> ! {
     loop {}
 }
 
-#[repr(packed)]
+#[allow(dead_code)]
+#[repr(C, packed)]
 struct LidtDesc {
     limit: u16,
     base: u32,
 }
 
-#[repr(packed)]
+#[allow(dead_code)]
+#[repr(C, packed)]
 #[derive(Copy, Clone)]
 struct IdtEntry {
     isr_low: u16,
@@ -29,12 +53,13 @@ struct IdtEntry {
 }
 
 #[no_mangle]
-unsafe fn i() {
+unsafe extern "C" fn i() {
     write_vga(b'I');
+    //pic_end_of_interrupt();
 }
 
 #[no_mangle]
-unsafe fn e() {
+unsafe extern "C" fn e() {
     write_vga(b'E');
 }
 
@@ -45,7 +70,19 @@ unsafe extern "C" fn exception_handler() -> ! {
 
 #[naked]
 unsafe extern "C" fn interrupt_handler() -> ! {
-    asm!("call i", "ret", options(noreturn));
+    asm!("pushad", "call i", "popad", "iretd", options(noreturn));
+}
+
+#[inline(always)]
+unsafe fn out8(port: u16, value: u8) {
+    asm!("out dx, al", in("al") value, in("dx") port)
+}
+
+#[inline(always)]
+unsafe fn in8(port: u16) -> u8 {
+    let value: u8;
+    asm!("in al, dx", out("al") value, in("dx") port);
+    value
 }
 
 unsafe fn write_vga(byte: u8) {
@@ -54,34 +91,31 @@ unsafe fn write_vga(byte: u8) {
     VGA_BUFFER = VGA_BUFFER.add(2);
 }
 
-unsafe fn setup_idt() {
-    const IDT_ENTRIES: u16 = 256;
-    const IDT_SIZE: u16 = core::mem::size_of::<IdtEntry>() as u16;
-    const IDT_LENGTH: u16 = IDT_ENTRIES * IDT_SIZE;
-    const CODE_SELECTOR_OFFSET: u16 = 8;
-
-    // First 32 set to exception handlers
-    let mut idt = [IdtEntry {
-        isr_low: (exception_handler as *const usize) as u16,
-        // The entry of our CODE selector in GDT
-        kernel_cs: CODE_SELECTOR_OFFSET,
-        reserved: 0,
-        attributes: 0x8E,
-        isr_high: ((exception_handler as *const usize as usize) >> 16) as u16,
-    }; IDT_ENTRIES as usize];
-
-    // Set the remaining to interrupt handlers
-    let mut counter = IDT_ENTRIES as usize - 1;
-    while counter > 32 {
-        idt[counter] = IdtEntry {
-            isr_low: (interrupt_handler as *const usize) as u16,
-            // The entry of our CODE selector in GDT
-            kernel_cs: CODE_SELECTOR_OFFSET,
-            reserved: 0,
-            attributes: 0x8E,
-            isr_high: ((interrupt_handler as *const usize as usize) >> 16) as u16,
-        };
-        counter -= 1;
+unsafe fn setup_idt(idt: &mut [IdtEntry; IDT_ENTRIES as usize]) {
+    let mut entry: usize = 0;
+    while entry < IDT_ENTRIES as usize {
+        if entry < 0x20 {
+            // 0x0..0x1F to exception handlers
+            idt[entry] = IdtEntry {
+                isr_low: (exception_handler as *const usize) as u16,
+                // The entry of our CODE selector in GDT
+                kernel_cs: CODE_SELECTOR_OFFSET,
+                reserved: 0,
+                attributes: 0x8F,
+                isr_high: ((exception_handler as *const usize as usize) >> 16) as u16,
+            };
+        } else {
+            // Rest interupt handlers
+            idt[entry] = IdtEntry {
+                isr_low: (interrupt_handler as *const usize) as u16,
+                // The entry of our CODE selector in GDT
+                kernel_cs: CODE_SELECTOR_OFFSET,
+                reserved: 0,
+                attributes: 0x8E,
+                isr_high: ((interrupt_handler as *const usize as usize) >> 16) as u16,
+            };
+        }
+        entry += 1;
     }
 
     let lidt_desc = LidtDesc {
@@ -90,15 +124,61 @@ unsafe fn setup_idt() {
     };
 
     asm!("lidt [{lidt_desc}]", lidt_desc = in(reg) &lidt_desc);
-    asm!("sti");
+}
+
+fn pic_end_of_interrupt() {
+    unsafe {
+        out8(PIC1_COMMAND, PIC_END_OF_INTERRUPT);
+        out8(PIC2_COMMAND, PIC_END_OF_INTERRUPT);
+    }
+}
+
+fn setup_pic() {
+    unsafe {
+        let pic1_mask = in8(PIC1_DATA);
+        let pic2_mask = in8(PIC2_DATA);
+
+        // Initialise the PIC
+        out8(PIC1_COMMAND, ICW1_INIT | ICW1_ICW4);
+        out8(PIC2_COMMAND, ICW1_INIT | ICW1_ICW4);
+
+        // Point the PIC to the IDT indices
+        out8(PIC1_DATA, 0x20);
+        out8(PIC2_DATA, 0x28);
+
+        // Tell master that slave is at IRQ2
+        out8(PIC1_DATA, 0b0000_0100);
+        // Tell slave its cascae identity
+        out8(PIC2_DATA, 0b0000_0010);
+
+        out8(PIC1_DATA, ICW4_8086);
+        out8(PIC2_DATA, ICW4_8086);
+
+        out8(PIC1_DATA, pic1_mask);
+        out8(PIC2_DATA, pic2_mask);
+    }
 }
 
 #[no_mangle]
 fn entry() {
     unsafe {
         write_vga(b'H');
-        setup_idt();
-        asm!("int 0x30");
+
+        let mut idt = [IdtEntry {
+            isr_low: 0,
+            kernel_cs: 0,
+            reserved: 0,
+            attributes: 0,
+            isr_high: 0,
+        }; IDT_ENTRIES as usize];
+        setup_idt(&mut idt);
+
+        //setup_pic();
+        //asm!("sti");
+        asm!("int 0x20");
+        asm!("int 0x20");
+        //asm!("int 100");
+        write_vga(b'Z');
+        asm!("hlt");
     }
-    loop {}
 }
